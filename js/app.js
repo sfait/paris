9dfc3da07021df045507a5d69cf18c73fdf3f6c1
Sylwia Fait <sylwia.fait@gmail.com>
2019-08-18 02:21:32 +0200

    Add subpages

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-// const init = () => {
-//     console.log("Hello World!");
-// };
-//
-// document.addEventListener("DOMContentLoaded", init);
+function showHamburgerMenu() {
+    const hamburger = document.querySelector(".hamburger");
+    const mainHeader  = document.querySelector(".main-header-subpage");
+    const menu = document.querySelector(".subpage-menu");
+
+    hamburger.addEventListener("click", function () {
+        hamburger.classList.toggle("active");
+        menu.classList.toggle("show-menu");
+        mainHeader.classList.toggle("hide-header");
+    })
+}
+
+const init = () => {
+    showHamburgerMenu();
+};
+
+document.addEventListener("DOMContentLoaded", init);
