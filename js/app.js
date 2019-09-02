29d4cba2a6a0f5c4c8e4f93de1770c5b37d29c2c
Sylwia Fait <sylwia.fait@gmail.com>
2019-09-02 22:21:09 +0200

    Update hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 function showHamburgerMenu() {
     const hamburger = document.querySelector(".hamburger");
-    const mainHeader  = document.querySelector(".main-header-subpage");
-    const menu = document.querySelector(".subpage-menu");
+    const menu = document.querySelector(".menu");
 
-    hamburger.addEventListener("click", function () {
-        hamburger.classList.toggle("active");
-        menu.classList.toggle("show-menu");
-        mainHeader.classList.toggle("hide-header");
-    })
+    const handleClick = () => {
+        hamburger.classList.toggle("hamburger-active");
+        menu.classList.toggle("menu-active");
+    }
+
+    hamburger.addEventListener("click", handleClick);
 }
 
 const init = () => {
